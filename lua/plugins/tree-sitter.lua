ee168b6589c38926925f647426b1897963fb5476
dongmo <mo.dong@unity.cn>
2026-04-27 17:00:51 +0800

    update tree-sitter, not using copilot

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -3,6 +3,16 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
+        "bash",
+        "c",
+        "cpp",
+        "python",
+        "rust",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "vim",
+        "vimdoc",
         "hlsl",
         "glsl",
       })
